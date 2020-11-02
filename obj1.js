c3288e170c1b2b4e82823398d4cbd4d73d03223c
Neha <nehaawachar22@gmail.com>
2020-11-02 22:20:22 +0530

    Day 4 Learning
    Accessing object properties

---
+++ obj1.js
@@ -0,0 +1,16 @@
+let person={
+
+firstName:"Neha",
+lastName:"Awachar",
+qualification:"BE",
+age:24,
+skills:"c,core java,manual testing,automation testing"
+}
+
+//Accessing Object Properties
+
+console.log(person.firstName);
+console.log(person.lastName);
+console.log(person.qualification);
+console.log(person.age);
+console.log(person.skills);
